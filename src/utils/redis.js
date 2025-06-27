ccbb00d1be17f3a484b0b53d3648a625f101157a
bao2206 <baopro2206@gmail.com>
2025-06-27 12:54:39 +0700

    Test deploy

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,9 +1,6 @@
-const Redis = require("ioredis");
+import Redis from "ioredis";
 
-const redis = new Redis({
-  host: "127.0.0.1", // Địa chỉ Redis
-  port: 6379, // Cổng Redis
-});
+const redis = new Redis(process.env.REDIS_URL);
 
 redis.on("connect", () => console.log("Kết nối Redis thành công!"));
 redis.on("error", (err) => console.error("Lỗi Redis:", err));
