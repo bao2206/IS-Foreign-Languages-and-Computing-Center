92c12e910cb46d8912ea6ab81da7e8f27166aa3f
bao2206 <baopro2206@gmail.com>
2025-06-27 14:10:34 +0700

    Add redis cloud

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,6 +1,13 @@
-import Redis from "ioredis";
+const Redis = require("ioredis");
+const dotenv = require('dotenv')
+dotenv.config();
 
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD 
+});
+
 
 redis.on("connect", () => console.log("Kết nối Redis thành công!"));
 redis.on("error", (err) => console.error("Lỗi Redis:", err));
